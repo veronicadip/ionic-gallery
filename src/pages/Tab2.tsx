bec8d9f673dd66f76cfcdbf1e0db185314c1154d
Vero <dipmveronica@gmail.com>
2024-05-02 21:17:23 -0300

    feat: component to take photos and storage them in the file system

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,23 +1,54 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonActionSheet,
+} from '@ionic/react';
+import {usePhotoGallery, UserPhoto} from '../hooks/usePhotoGallery';
+import {useEffect, useState} from "react";
 
 const Tab2: React.FC = () => {
+    const { takePhoto, photos } = usePhotoGallery();
+
+    useEffect(() => {
+        console.log(photos)
+    }, [photos]);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    {photos.map((photo, index) => (
+                        <IonCol size="6" key={photo.filepath}>
+                            <IonImg src={photo.webviewPath} />
+                        </IonCol>
+                    ))}
+                </IonRow>
+            </IonGrid>
+            <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                <IonFabButton onClick={() => takePhoto()}>
+                    <IonIcon icon={camera}></IonIcon>
+                </IonFabButton>
+            </IonFab>
+        </IonContent>
     </IonPage>
   );
 };
